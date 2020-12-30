a5f9c54c84be32ccb209e4a7410a0a4d688301fa
codeliuzhan <liuzhan1999@163.com>
2020-12-30 15:31:51 +0800

    把本地工程添加到commit

---
+++ git-test/src/cn/itcast/java/Hello.java
@@ -0,0 +1,18 @@
+package cn.itcast.java;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("你好 GIT");
+        System.out.println("你好 GIT");
+        System.out.println("你好 GIT");
+        System.out.println("你好 GIT");
+        System.out.println("你好 GIT");
+        System.out.println("你好 GIT");
+        System.out.println("你好 GIT");
+        System.out.println("你好 GIT");
+        System.out.println("你好 GIT");
+        System.out.println("你好 GIT");
+    }
+
+
+}
